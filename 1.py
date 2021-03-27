49539e6d143404a84e62f0d96defd8c7a37eba1b
Anselmo-2010 <you@example.com>
2021-03-27 18:43:04 +0600

    added dz 22 and 23

---
+++ 1.py
@@ -0,0 +1,35 @@
+from random import randint
+
+print("Задание-1")
+
+def rev_Num():
+    num = int(input("Введите ваше число от 100 до 1000 : "  ))
+    if num > 100 and num < 1000:
+        lastNum = num % 10
+        secondNum = num // 10
+        secondNum_2 = secondNum % 10
+        firstNum = secondNum // 10
+        return lastNum * 100 + secondNum_2 * 10 + firstNum    
+    else:
+        return "incorrect number, try again"
+
+mynum = rev_Num()
+print(mynum)
+
+print("----------------------------------------------------------")
+
+
+
+print("Задание-2")
+
+num_len = int(input(" Input please length of the list : "))
+lst = []
+for i in range(num_len):
+    r_num = randint(0, 10)
+    lst.append(r_num)
+
+
+new_lst = list(map(lambda x: x ** 2, lst))
+
+print(lst)
+print(new_lst)
